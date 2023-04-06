bd69f33e11351b817efcc76fa19c557807da315c
eundo2 <ehdms2324@naver.com>
2023-04-06 14:07:04 +0900

    26. 패스워드 암호화

--- src/main/java/com/example/todo/controller/UserController.java
+++ src/main/java/com/example/todo/controller/UserController.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,13 +27,15 @@ public class UserController {
 	@Autowired
 	private TokenProvider tokenProvider;
 	
+	private PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+	
 	@PostMapping("/signup")
 	public ResponseEntity<?>registerUser(@RequestBody UserDTO userDTO){
 		try {
 			UserEntity user = UserEntity.builder()
 					.email(userDTO.getEmail())
 					.username(userDTO.getUsername())
-					.password(userDTO.getPassword())
+					.password(passwordEncoder.encode(userDTO.getPassword()))
 					.build();
 			
 			UserEntity registeredUser = userService.create(user);
@@ -44,17 +45,17 @@ public class UserController {
 					.username(registeredUser.getUsername())
 					.build();
 			return ResponseEntity.ok().body(responseUserDTO);
-		}catch(Exception e){
+		} catch(Exception e) {
 			ResponseDTO responseDTO = ResponseDTO.builder().error(e.getMessage()).build();
 			return ResponseEntity.badRequest().body(responseDTO);
 		}
 	}
 
 	@PostMapping("/signin")
-	public ResponseEntity<?>authenticate(@RequestBody UserDTO userDTO){
-		UserEntity user = userService.getByCredentials(userDTO.getEmail(), userDTO.getPassword());
+	public ResponseEntity<?>authenticate(@RequestBody UserDTO userDTO) {
+		UserEntity user = userService.getByCredentials(userDTO.getEmail(), userDTO.getPassword(), passwordEncoder);
 		
-		if(user !=null){
+		if(user!=null) {
 			final String token = tokenProvider.create(user);
 			final UserDTO responseUserDTO = UserDTO.builder()
 					.email(user.getEmail())
