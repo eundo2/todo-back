7c7baf254f487a7d18dfb4eb9f2d125f7ec205ab
eundo2 <ehdms2324@naver.com>
2023-04-06 14:36:12 +0900

    27. Spring Security 비활성화

--- src/main/java/com/example/todo/TodoApplication.java
+++ src/main/java/com/example/todo/TodoApplication.java
@@ -3,6 +3,8 @@ package com.example.todo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// exclude 주석해제 + 기존거 주석 + websecurityconfig에서 @enablewebsecurity 주석처리 + 새todocontroller에;서 temporary-userid 주석해제
+//@SpringBootApplication(exclude = org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class)
 @SpringBootApplication
 public class TodoApplication {
 
--- src/main/java/com/example/todo/controller/TodoController.java
+++ src/main/java/com/example/todo/controller/TodoController.java
@@ -35,43 +35,46 @@ public class TodoController {
 			@AuthenticationPrincipal String userId,
 			@RequestBody TodoDTO dto) {
 		try {
-		/* POST localhost:8080/todo
-		 * {
-		 * 		"title" : "My first todo",
-		 * 		"done" : false
-		 * }
-		 */
-
-		// dto 를 이용해 테이블에 저장하기 위한 entity를 생성한다.
-		TodoEntity entity = TodoDTO.toEntity(dto);
-		// entity userId를 임시로 지정한다.
-		entity.setId(null);
-		entity.setUserId(userId);
-		
-		// service.create 를 통해 repository 에 entity를 저장한다.
-		// 이때 넘어노는 값이 없을 수도 있으므로 List가 아닌 Optional 로 한다.
-		List<TodoEntity> entities = service.create(entity);
-		
-		// entities 를 dtos 로 스트림 변환한다.
-		List<TodoDTO> dtos = entities.stream().map(TodoDTO::new).collect(Collectors.toList());
-		log.info("Log:entities => dtos ok!");		
-
-		// Response DTO를 생성한다.
-		/* {
-		 * 		"error" : null,
-		 * 		"data": [
-		 * 			{
-		 * 				"id" : "402809817ed71ddf017ed71dfe720000",
-		 * 				"title" : "My first todo",
-		 * 				"done" : false
-		 * 			}
-		 * } 
-		 */
-		ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().data(dtos).build();
-		log.info("Log:responsedto ok!");
-		
-		// HTTP Status 200 상태로 response 를 전송한다.
-		return ResponseEntity.ok().body(response);
+			// 스프링 시큐리티 비활성화시 주석해제 
+			// userId="temporary-userid";
+			
+			/* POST localhost:8080/todo
+			 * {
+			 * 		"title" : "My first todo",
+			 * 		"done" : false
+			 * }
+			 */
+	
+			// dto 를 이용해 테이블에 저장하기 위한 entity를 생성한다.
+			TodoEntity entity = TodoDTO.toEntity(dto);
+			// entity userId를 임시로 지정한다.
+			entity.setId(null);
+			entity.setUserId(userId);
+			
+			// service.create 를 통해 repository 에 entity를 저장한다.
+			// 이때 넘어노는 값이 없을 수도 있으므로 List가 아닌 Optional 로 한다.
+			List<TodoEntity> entities = service.create(entity);
+			
+			// entities 를 dtos 로 스트림 변환한다.
+			List<TodoDTO> dtos = entities.stream().map(TodoDTO::new).collect(Collectors.toList());
+			log.info("Log:entities => dtos ok!");		
+	
+			// Response DTO를 생성한다.
+			/* {
+			 * 		"error" : null,
+			 * 		"data": [
+			 * 			{
+			 * 				"id" : "402809817ed71ddf017ed71dfe720000",
+			 * 				"title" : "My first todo",
+			 * 				"done" : false
+			 * 			}
+			 * } 
+			 */
+			ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().data(dtos).build();
+			log.info("Log:responsedto ok!");
+			
+			// HTTP Status 200 상태로 response 를 전송한다.
+			return ResponseEntity.ok().body(response);
 		} catch (Exception e) {
 			String error = e.getMessage();
 			ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().error(error).build();
@@ -81,6 +84,8 @@ public class TodoController {
 	
 	@GetMapping
 	public ResponseEntity<?> retrieveTodo(@AuthenticationPrincipal String userId) {
+		// 스프링 시큐리티 비활성화시 주석해제 
+		// userId="temporary-userid";			
 		
 		List<TodoEntity> entities = service.retrieve(userId);
 		List<TodoDTO> dtos = entities.stream().map(TodoDTO::new).collect(Collectors.toList());
@@ -98,31 +103,35 @@ public class TodoController {
 			@AuthenticationPrincipal String userId,
 			@RequestBody TodoDTO dto) {
 		try {
-		/* PUT localhost:8080/todo
-		 * {
-		 * 		"id" : "????????"
-		 * 		"title" : "Update first todo",
-		 * 		"done" : true
-		 * }
-		 */
-		// dto 를 이용해 테이블에 저장하기 위한 entity를 생성한다.
-		TodoEntity entity = TodoDTO.toEntity(dto);
-
-		// entity userId를 임시로 지정한다.
-		entity.setUserId(userId);
-		
-		// service.create 를 통해 repository 에 entity를 저장한다.
-		// 이때 넘어노는 값이 없을 수도 있으므로 List가 아닌 Optional 로 한다.
-		List<TodoEntity> entities = service.update(entity);
-		
-		// entities 를 dtos 로 스트림 변환한다.
-		List<TodoDTO> dtos = entities.stream().map(TodoDTO::new).collect(Collectors.toList());
+			// 스프링 시큐리티 비활성화시 주석해제 
+			// userId="temporary-userid";
+						
+				
+			/* PUT localhost:8080/todo
+			 * {
+			 * 		"id" : "????????"
+			 * 		"title" : "Update first todo",
+			 * 		"done" : true
+			 * }
+			 */
+			// dto 를 이용해 테이블에 저장하기 위한 entity를 생성한다.
+			TodoEntity entity = TodoDTO.toEntity(dto);
 	
-		// Response DTO를 생성한다.
-		ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().data(dtos).build();
+			// entity userId를 임시로 지정한다.
+			entity.setUserId(userId);
+			
+			// service.create 를 통해 repository 에 entity를 저장한다.
+			// 이때 넘어노는 값이 없을 수도 있으므로 List가 아닌 Optional 로 한다.
+			List<TodoEntity> entities = service.update(entity);
+			
+			// entities 를 dtos 로 스트림 변환한다.
+			List<TodoDTO> dtos = entities.stream().map(TodoDTO::new).collect(Collectors.toList());
 		
-		// HTTP Status 200 상태로 response 를 전송한다.
-		return ResponseEntity.ok().body(response);
+			// Response DTO를 생성한다.
+			ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().data(dtos).build();
+			
+			// HTTP Status 200 상태로 response 를 전송한다.
+			return ResponseEntity.ok().body(response);
 		} catch (Exception e) {
 			String error = e.getMessage();
 			ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().error(error).build();
@@ -135,6 +144,9 @@ public class TodoController {
 			@AuthenticationPrincipal String userId,
 			@RequestBody TodoDTO dto) {
 		try {
+			// 스프링 시큐리티 비활성화시 주석해제 
+			// userId="temporary-userid";
+						
 			TodoEntity entity = TodoDTO.toEntity(dto);
 
 			// entity userId를 임시로 지정한다.
@@ -150,11 +162,11 @@ public class TodoController {
 			
 			// HTTP Status 200 상태로 response 를 전송한다.
 			return ResponseEntity.ok().body(response);
-			} catch (Exception e) {
-				String error = e.getMessage();
-				ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().error(error).build();
-				return ResponseEntity.badRequest().body(response);
-			}			
+		} catch (Exception e) {
+			String error = e.getMessage();
+			ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().error(error).build();
+			return ResponseEntity.badRequest().body(response);
+		}			
 	
 	}
 }
